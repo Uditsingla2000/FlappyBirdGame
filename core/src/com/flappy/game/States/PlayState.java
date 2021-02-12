91896ca140a29a7b23966fc856c2cfd241781a81
Uditsingla2000 <69379767+Uditsingla2000@users.noreply.github.com>
2021-02-12 22:41:48 +0530

    Phase 8

--- core/src/com/flappy/game/States/PlayState.java
+++ core/src/com/flappy/game/States/PlayState.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.flappy.game.FlappyDemo;
 import com.flappy.game.sprites.Bird;
+import com.flappy.game.sprites.Tube;
 
 public class PlayState extends state {
      private Bird bird;
      private Texture bg;
+     private Tube tube;
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50,300);
         cam.setToOrtho(false, FlappyDemo.WIDTH/2,FlappyDemo.HEIGHT/2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -36,6 +39,9 @@ public class PlayState extends state {
       sb.begin();
       sb.draw(bg,cam.position.x - (cam.viewportWidth/2),0);
       sb.draw(bird.getTexture(),bird.getPosition().x,bird.getPosition().y);
+      sb.draw(tube.getTopTube(),tube.getPosTopTube().x,tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(),tube.getPosBotTube().x,tube.getPosTopTube().y);
+
       sb.end();
     }
 
