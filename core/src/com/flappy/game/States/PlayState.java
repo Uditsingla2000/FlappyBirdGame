c1b76bce6b0c51f9575b9f5d35d98f9ee5f14730
Agarwal21SK <62434208+Agarwal21SK@users.noreply.github.com>
2021-02-13 13:33:33 +0530

    Phase-10

--- core/src/com/flappy/game/States/PlayState.java
+++ core/src/com/flappy/game/States/PlayState.java
@@ -3,20 +3,32 @@ package com.flappy.game.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.flappy.game.FlappyDemo;
 import com.flappy.game.sprites.Bird;
 import com.flappy.game.sprites.Tube;
 
+
+
+
 public class PlayState extends state {
+    private static final int TUBE_SPACING=125;
+    private static final int TUBE_COUNT=4;
      private Bird bird;
      private Texture bg;
      private Tube tube;
+     private Array<Tube> tubes;
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50,300);
         cam.setToOrtho(false, FlappyDemo.WIDTH/2,FlappyDemo.HEIGHT/2);
         bg = new Texture("bg.png");
         tube = new Tube(100);
+
+        tubes = new Array<Tube>();
+        for(int i=1;i<=TUBE_COUNT;i++){
+            tubes.add(new Tube(i*(TUBE_SPACING+Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
@@ -30,7 +42,16 @@ public class PlayState extends state {
     public void update(float dt) {
         handleInput();
         bird.update(dt);
+        cam.position.x=bird.getPosition().x + 80;
+        for(Tube tube: tubes){
+            if(cam.position.x-(cam.viewportWidth/2)>tube.getPosTopTube().x+tube.getTopTube().getWidth()){
+                tube.reposition(tube.getPosTopTube().x +((Tube.TUBE_WIDTH +TUBE_SPACING)*TUBE_COUNT));
+            }
+            if(tube.collides(bird.getBounds()))
+                gsm.set(new PlayState(gsm));
 
+        }
+        cam.update();
     }
 
     @Override
@@ -40,7 +61,7 @@ public class PlayState extends state {
       sb.draw(bg,cam.position.x - (cam.viewportWidth/2),0);
       sb.draw(bird.getTexture(),bird.getPosition().x,bird.getPosition().y);
       sb.draw(tube.getTopTube(),tube.getPosTopTube().x,tube.getPosTopTube().y);
-        sb.draw(tube.getBottomTube(),tube.getPosBotTube().x,tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);
 
       sb.end();
     }
--- core/src/com/flappy/game/sprites/Bird.java
+++ core/src/com/flappy/game/sprites/Bird.java
@@ -1,14 +1,17 @@
 package com.flappy.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class Bird {
 
 
     private static final int GRAVITY = -15;
+    private static final int MOVEMENT = 100;
     private Vector3 position;
     private Vector3 velocity;
+    private Rectangle bounds;
 
     private Texture bird;
 
@@ -16,16 +19,20 @@ public class Bird {
         position = new Vector3(x , y,0);
         velocity = new Vector3(0,0,0);
         bird = new Texture("bird.png");
+        bounds = new Rectangle(x,y, bird.getWidth(), bird.getHeight());
+
     }
 
     public void update(float dt){
         if (position.y>0)
              velocity.add(0,GRAVITY,0);
         velocity.scl(dt);
-        position.add(0,velocity.y,0);
+        position.add(MOVEMENT*dt,velocity.y,0);
         if (position.y<0)
             position.y = 0;
         velocity.scl(1/dt);
+        bounds.setPosition(position.x,position.y);
+
 
     }
 
@@ -40,5 +47,7 @@ public class Bird {
     public void jump(){
         velocity.y = 250;
     }
-
+    public Rectangle getBounds(){
+        return bounds;
+    }
 }
